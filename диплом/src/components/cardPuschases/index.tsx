96d25a34b3032e9267969e8f9d8af5c9cb4af0f9
mrskrisss <150622099+mrskrisss@users.noreply.github.com>
2024-06-26 12:09:00 +0300

    bookstore: correcting component styles

--- диплом/src/components/cardPuschases/index.tsx
+++ диплом/src/components/cardPuschases/index.tsx
@@ -5,7 +5,6 @@ import { ICardBook } from '../../types/ICardBook'
 import { toggleFavoriteById } from '../../redux/book-slice'
 import { Counter } from '../counter'
 import Close from '../../icons/close'
-
 import './index.scss'
 
 export const CardPurchases = (props: ICardBook) => {
@@ -23,22 +22,26 @@ export const CardPurchases = (props: ICardBook) => {
             <img className="img-favorite" src={props.image} />
         </div>
         <div className="card-information-favorite">
-            <div className="card-name-favorite">
-                <p className="name-book-favorite">{props.title}</p>
+            <div className="field-card-left">
+                <div className="card-name-favorite">
+                    <p className="name-book-favorite">{props.title}</p>
+                </div>
+                <div className="card-description-favorite">
+                    <p className="author-and-year-favorite">{props.subtitle}</p>
+                </div>
+                <div className="card-counter">
+                    <Counter />
+                </div>
             </div>
-            <div className="card-description-favorite">
-                <p className="author-and-year-favorite">{props.subtitle}</p>
+            <div className="field-card-right">
+                <div className="card-price-favorite">
+                    <p className="price-favorite">{props.price}</p>
+                </div>
+                <button className="button-close" onClick={() => { handleClickRemoveFromPurchases(props.isbn13) }}>
+                    <Close />
+                </button>
             </div>
-            <div className="card-counter">
-                <Counter />
-            </div>
-            {/* <div className="card-price-favorite">
-                <p className="price-favorite">{props.price}</p>
-            </div> */}
         </div>
-        <button className="button-close" onClick={() => { handleClickRemoveFromPurchases(props.isbn13) }}>
-            <Close />
-        </button>
     </div>
   )
 }
---
+++ диплом/src/icons/minus.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Minus: React.FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-dash-lg" viewBox="0 0 16 16">
+        <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8"/>
+    </svg>
+  )
+}
+
+export default Minus
